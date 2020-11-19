e8d667e545a727d012384f44fba507a06ba3f63e
albanceku <4m3895@e>
2020-11-19 23:57:37 +0100

    changes made for Steg 2

---
+++ asignment/lib/SecondView.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'model.dart';
+
+class SecondView extends StatelessWidget {
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TIG169 TO DO'),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _nameLabel(),
+            _nameInputField(),
+            Container(
+              height: 24,
+            ),
+            Container(
+              height: 24,
+            ),
+            _buttonRow(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _nameLabel() {
+    return Container(
+      margin: EdgeInsets.only(
+        left: 16,
+        top: 24,
+      ),
+      child: Text(''),
+    );
+  }
+
+//fältet där Todo skrivs in
+  Widget _nameInputField() {
+    return Container(
+      margin: EdgeInsets.all(30),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'What are you going to do?',
+        ),
+        controller: textEditingController,
+      ),
+    );
+  }
+
+  Widget _buttonRow(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RaisedButton(
+          onPressed: () {
+            Provider.of<MyState>(context, listen: false)
+                .addItem(Item(name: textEditingController.text));
+            Navigator.popAndPushNamed(context, "/");
+          },
+          child: Text('+' + ' ' + 'Add'),
+          color: Colors.white,
+          textColor: Colors.black,
+          highlightColor: Colors.amber,
+        ),
+      ],
+    );
+  }
+}
